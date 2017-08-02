30ecb153c566ca46b89ade317fc45963768ab2dc
WilkeAleksandra <olawilke@wp.pl>
2017-08-02 18:28:32 +0200

    Add files via upload

---
+++ Hoodie/Assets/Users.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Users : MonoBehaviour
+{
+
+
+    private const int MAX_PLACES = 5;
+    private const int NICK_X = 820;
+    private const int POSITION_Y = 400;
+    private const int POINTS_X = 1230;
+    private const int HEIGHT = 50;
+    private const int WIDTH = 100;
+    private const int GAP = 60;
+    private GUIStyle style = new GUIStyle();
+
+    void Start()
+    {
+
+        style.fontSize = 50;
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    void OnGUI()
+    {
+        int moveDownClear = 0;
+        for (int i = 0; i < UsersList.howManyUsers - 1; i++)
+        {
+            GUI.Box(new Rect(NICK_X, POSITION_Y + moveDownClear, WIDTH, HEIGHT), "", style);
+            GUI.Box(new Rect(POINTS_X, POSITION_Y + moveDownClear, WIDTH, HEIGHT), "", style);
+            moveDownClear += GAP;
+        }
+        UsersList.usersList.Sort();
+        int moveDown = 0;
+        int nrPlace = 1;
+        foreach (User user in UsersList.usersList)
+        {
+
+            GUI.Box(new Rect(NICK_X, POSITION_Y + moveDown, WIDTH, HEIGHT), nrPlace.ToString() + ". " + user.getNickname(), style);
+            GUI.Box(new Rect(POINTS_X, POSITION_Y + moveDown, WIDTH, HEIGHT), user.getPoints().ToString(), style);
+            moveDown += GAP;
+            nrPlace++;
+            if (nrPlace > MAX_PLACES)
+            {
+                break;
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+
---
+++ Hoodie/Assets/UsersList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsersList : MonoBehaviour {
+
+    public static List<User> usersList = new List<User>();
+    public static int howManyUsers = 0;
+    private User user;
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+
+        if (DataInfoClass.addUser)
+        {
+            addToList();
+            DataInfoClass.addUser = false;
+        }
+
+    }
+    public void addToList()
+    {
+        howManyUsers++;
+        user = new User();
+        user.setNickname( NameSaveOnClick.userName);
+        user.setPoints( Player.points);
+        usersList.Add(user);
+
+    }
+}
